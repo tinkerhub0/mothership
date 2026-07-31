adc1e52f3937a166dd1960021336dffcda4e7de6
sinanmohd <sinan@sinanmohd.com>
2026-08-01 00:36:32 +0530

    user-vms: init sinan

---
+++ user-vms/sinan.nix
@@ -0,0 +1,9 @@
+{
+  enabled = true;
+  github = "sinanmohd";
+  tier = "large";
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJnqxnFFvG1R/ZfP9GjRem3V9GfSK5pM1uEmsQZrMGWX sinan@exy"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8LnyOuPmtKRqAZeHueNN4kfYvpRQVwCivSTq+SZvDU sinan@cez"
+  ];
+}
