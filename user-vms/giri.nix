847bcefa8f9da4a229de2c30a808625a9da92c85
grootgeko-rgb <grootgeko@gmail.com>
2026-07-31 10:49:13 +0530

    giri.nix

--- user-vms/giri.nix
+++ user-vms/giri.nix
@@ -1,6 +1,6 @@
 {
-  github = "fahshad-ai";
-  tier = "large";
+  github = "grootgeko-rgb";
+  tier = "medium";
   enabled = true;
   keys = [
        "SHA256:QN2/4JiIyePir2xY7HxCGv8ey49rcAdccX8q/suARTU grootgeko@gmail.com"
