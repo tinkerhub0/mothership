1678ea7f2aea0c1b10cc691a3e8e6510add0554f
Ashish Shaiju <74810128+ashishshaiju@users.noreply.github.com>
2026-08-01 02:03:02 +0530

    Add user configuration

---
+++ user-vms/ashishshaiju.nix
@@ -0,0 +1,8 @@
+{
+  github = "ashishshaiju";
+  tier = "large";
+  enabled = true;
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMDGNzwrI5VTERQLF7GXXSb8wEfZNXbNEB3OS9ZpuTmH ashishshaiju@tharavad"
+  ];
+}
