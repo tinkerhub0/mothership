9f65580b68ae5c024edf76556003fdebf2c74721
Ashish Shaiju <74810128+ashishshaiju@users.noreply.github.com>
2026-08-01 03:06:33 +0530

    Configure publish settings for ashishshaiju
    Added publish configuration for subdomains and ports.

--- user-vms/ashishshaiju.nix
+++ user-vms/ashishshaiju.nix
@@ -5,4 +5,9 @@
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMDGNzwrI5VTERQLF7GXXSb8wEfZNXbNEB3OS9ZpuTmH ashishshaiju@tharavad"
   ];
+  publish = [
+    { port = 3000; }
+    { subdomain = "app-ashishshaiju"; port = 3005; }
+    { subdomain = "api-ashishshaiju"; port = 5000; }
+  ];
 }
