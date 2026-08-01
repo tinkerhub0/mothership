7d34faf994e0e908e31790ecb3b5d170864469a3
Arjun Krishna <arjun12345krishna@gmail.com>
2026-08-01 06:42:51 +0530

    member: add arjun (medium)

---
+++ user-vms/arjun.nix
@@ -0,0 +1,13 @@
+{
+  github = "decoded-cipher";
+  tier = "medium";
+  enabled = true;
+
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHYPYu7eGEg6b4LrRrAa8V7qe7beBW9WY0FP3nKX6sXx arjun@tharavad"
+  ];
+
+  publish = [
+    { port = 3000; }
+  ];
+}
