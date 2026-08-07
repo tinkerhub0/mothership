9ca06b32b6c86a46b19c17718f3d1e1f09147497
Ram Uday <ramudaynair@gmail.com>
2026-08-07 16:26:36 +0530

    add ramudaynair

---
+++ user-vms/ramudaynair.nix
@@ -0,0 +1,11 @@
+# member: ramudaynair
+{
+  github = "ramudaynair";
+  tier = "small";
+  enabled = true;
+
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKE1Kt0YwVwUV58bF0u1G1YeojmKhhwNYnSTaMVi/ksf Ram Uday"
+  ];
+
+}
