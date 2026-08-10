476cd0ca0f5306f94b62310146e7474833d16aa2
Sabari Vijayan <vijayansabari06@gmail.com>
2026-08-10 22:22:31 +0530

    Add sabari.nix configuration for user VM
    Adding my name to vms-users directory

---
+++ user-vms/sabari.nix
@@ -0,0 +1,9 @@
+{
+  github = "Sabari-Vijayan";
+  tier = "small";
+  enabled = true;
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2Y3Rk0yD9paxmNks9q9W7nCysoiVgJtuIr5X/BoDiC vijayansabari06@gmail.com"
+  ];
+  publish = [ { port = 5456; } ];
+}
