e6d0a18e9622b3ba94877b870b12937b376a465e
HUSSAINkm91 <167412566+HUSSAINkm91@users.noreply.github.com>
2026-08-10 10:50:05 +0530

    Add configuration for user 'hussain' in Nix file

---
+++ user-vms/hussain.nix
@@ -0,0 +1,8 @@
+{
+  github = " HUSSAINkm91";
+  tier = "small";
+  enabled = true;
+  keys = [
+       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCb3syKVoHES2TyOkWP3vJabx6Qc8hXD/boiFS3x9g6 hussainkm91@gmail.com"
+  ];
+}
