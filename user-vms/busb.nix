04dfd68bc576d73b3d42f2b23be9b9e42d3b91f5
Kevin Babu <116744347+kevin-babu-dotcom@users.noreply.github.com>
2026-08-16 01:30:26 +0530

    Add busb VM

---
+++ user-vms/busb.nix
@@ -0,0 +1,9 @@
+{
+  github = "kevin-babu-dotcom";
+  tier = "small";
+  enabled = true; 
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6K5bh7e3pyvcyr/TbhgeRUXr06zTjMlIAImh0eN/mC kevin"
+  ];
+   publish = [ { port = 3000; } ];
+}
