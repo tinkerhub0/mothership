d61ec62c02ccf5f4603fe1c95113b233867aab96
Rishi-k-s <rishikrishna.sr@gmail.com>
2026-08-18 11:42:25 +0530

    added a new user rishi(njan) with ssh keys to user-vms

---
+++ user-vms/rishi.nix
@@ -0,0 +1,9 @@
+{
+  tier = "large";
+  enabled = true;
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFEt8v0Fd9DD0ONfueo+sL/oWHM/60qcO04hUBF2vox7"
+  ];
+  # optional:
+  # publish = [ { port = 3000; } ];
+}
